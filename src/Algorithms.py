2e4442a0cf42d4e3d6ce0459db0005c8f3a41a43
Henrique Cordeiro <h.cordeirop@gmail.com>
2023-08-06 21:38:16 -0300

    quicksort

--- src/Algorithms.py
+++ src/Algorithms.py
@@ -1,5 +1,5 @@
 class Algorithms:
-    def bubblesort(self, array):
+    def bubblesort(self, array): # comparison in pairs
         for i in range(len(array)-1):
             for j in range(len(array)-1):
 
@@ -7,7 +7,7 @@ class Algorithms:
                 if array[j] > array[j+1]:
                     array[j], array[j+1] = array[j+1], array[j]          
 
-    def insertionsort(self, array):
+    def insertionsort(self, array): # verify each one
         for i in range(len(array)):
 
             # select starting index
@@ -18,9 +18,8 @@ class Algorithms:
             while j > 0 and array[j] < array[j-1]:
                 array[j-1], array[j] = array[j], array[j-1]
                 j -= 1
-        return array
 
-    def selectionsort(self, array):
+    def selectionsort(self, array): # pick and swap
         for i in range(len(array)):
             
             # select item to be compared
@@ -35,22 +34,26 @@ class Algorithms:
 
                 # switch smaller item with currently selected    
             array[min_index], array[i] = array[i], array[min_index]
-        return array
 
-    def mergesort(self, array):
+    def mergesort(self, array): # divide and conquer
         if len(array)>1:
                 
+            # find middle index
             mid = len(array) // 2
 
+            # divide the array into two equal parts
             left = array[:mid]
             right = array[mid:]
 
+            # recursively repeat the process
             self.mergesort(left)
             self.mergesort(right)
 
             i = j = k = 0
             
             while i < len(left) and j < len(right):
+                
+                # compare values in both sides and add the smallest to the final array
                 if left[i] < right[j]:
                     array[k] = left[i]
                     i += 1
@@ -59,6 +62,7 @@ class Algorithms:
                     j += 1
                 k += 1
 
+            # make sure there aren't values left to be compared
             while i < len(left):
                 array[k] = left[i]
                 i += 1
@@ -69,7 +73,33 @@ class Algorithms:
                 j += 1
                 k += 1
 
-        return array
     
-    
+    def quicksort(self, array, low, high): # partition and pivot
+        if low < high:
+           
+            # pick the pivot item
+            pivot = self._partition(array, low, high)
+
+            # recursively call the function dividing on the pivot
+            self.quicksort(array, low, pivot - 1)
+            self.quicksort(array, pivot + 1, high)
+        
+
+    def _partition(self, array, low, high):
+
+        # set the pivot as the last item in the current array
+        pivot = array[high]
+        
+        i = low - 1
+
+        for j in range(low, high):
+            if array[j] <= pivot:
+                i = i + 1
+
+                array[i], array[j]  = array[j], array[i]
+
+
+        array[i + 1], array[high] = array[high], array[i + 1]
+
+        return i + 1 
 
